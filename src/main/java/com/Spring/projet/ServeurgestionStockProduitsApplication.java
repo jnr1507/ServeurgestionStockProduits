0836909e9fa49615436c87093945bdeea62dd3aa
jnr1507 <jnralene@gmail.com>
2020-05-23 01:34:47 -0700

    first commit

---
+++ src/main/java/com/Spring/projet/ServeurgestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurgestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurgestionStockProduitsApplication.class, args);
+	}
+
+}
